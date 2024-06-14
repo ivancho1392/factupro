a8aa0fabb678eae57198d391f1cbc91fb686c247
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2024-06-14 06:55:04 -0500

    Add footer

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,14 +4,16 @@ import React, { useState } from 'react';
 import Topbar from '../components/topbar';
 import InvoiceConsult from '../components/invoiceConsult';
 import InvoiceUpload from '../components/invoiceUpload';
+import { Montserrat } from 'next/font/google';
 
+const montserrat = Montserrat({ subsets: ['latin'] });
 const Home: React.FC = () => {
   const [activeComponent, setActiveComponent] = useState<'consult' | 'upload'>('consult');
 
   return (
-    <div>
+    <div className={montserrat.className}>
       <Topbar setActiveComponent={setActiveComponent} />
-      <div className="w-full flex flex-col lg:flex-row gap-4">
+      <div className="w-full flex flex-col lg:flex-row gap-4 mb-[60px]">
         <div className="w-full lg:w-[70%]">
           {activeComponent === 'consult' && <InvoiceConsult />}
           {activeComponent === 'upload' && <InvoiceUpload />}
