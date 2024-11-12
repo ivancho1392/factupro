4b2c04cd0fbf9a3791028affae82a025964bc1a9
JorgeIvanPuyo <ji.puyo@uniandes.edu.co>
2024-11-11 19:52:52 -0500

    Fix deploy

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -1,13 +1,19 @@
 import React, { useContext } from "react";
 import { FaChevronDown } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
-import MenuVisibilityContext  from "../../context/menuVisibilityContext";
+import MenuVisibilityContext from "../../context/menuVisibilityContext";
 import styles from "../../styles/sidebar.module.css";
 
-const Sidebar = ({ activeComponent, setActiveComponent }) => {
+// Define los tipos de los props
+interface SidebarProps {
+  activeComponent: "upload" | "consult" | null;
+  setActiveComponent: (component: "upload" | "consult" | null) => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ activeComponent, setActiveComponent }) => {
   const { menuVisible, toggleMenu } = useContext(MenuVisibilityContext);
 
-  const handleOptionClick = (option) => {
+  const handleOptionClick = (option: "upload" | "consult" | null) => {
     setActiveComponent(option);
     if (window.innerWidth <= 768) {
       toggleMenu(); // Cierra el menú en mobile después de seleccionar una opción
